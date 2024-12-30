7486f86f2e8f6af374d903eb150784369540bbd8
kaustav3071 <156155730+kaustav3071@users.noreply.github.com>
2024-12-30 17:20:38 +0530

    new_files

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-#ok ok
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Household Services App!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
